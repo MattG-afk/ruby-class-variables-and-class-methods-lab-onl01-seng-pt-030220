25543383b74ad601c02ef437e8360b7c6ed89b9c
Matt Gordon <bigmattsize21@gmail.com>
2020-03-23 01:58:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,39 +1,39 @@
 class Song
 attr_accessor :name, :artist, :genre
-  @count = 0
-  @artists = []
-  @genres = []
+  @@count = 0
+  @@artists = []
+  @@genres = []
 
   def initialize(song_name,artist,genre)
     @name = song_name
     @artist = artist
     @genre = genre
-    @count += 1
-    @artists << artist
-    @genres << genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
   end
 
   def self.count 
-    @count
+    @@count
   end
 
   def self.artists
-    @artists.uniq
+    @@artists.uniq
   end
 
   def self.genres
-    @genres.uniq
+    @@genres.uniq
   end
 
   def self.genre_count
     genre_count = Hash.new(0)
-    @genres.each {|genre| genre_count[genre] += 1}
+    @@genres.each {|genre| genre_count[genre] += 1}
     genre_count
   end
 
   def self.artist_count
     artist_count = Hash.new(0)
-    @artists.each {|artist| artist_count[artist] += 1}
+    @@artists.each {|artist| artist_count[artist] += 1}
     artist_count
   end
 
